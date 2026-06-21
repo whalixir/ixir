fc0301abf4d01578858b14b8b99aac9130704005
whalixir <r4t9p55fg2@privaterelay.appleid.com>
2026-06-21 08:46:52 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='wx-v2';
+const V='wx-v3';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>{
   e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))));
